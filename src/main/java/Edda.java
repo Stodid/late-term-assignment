c4608d1b9619df0ba21c7bd27c140414914f6e9b
Palli Moon <gunnarpg13@ru.is>
2014-10-26 21:50:40 +0000

    Basic gradle set up and passing test case added

---
+++ src/main/java/Edda.java
@@ -0,0 +1,9 @@
+package edda;
+
+public class Edda {
+    public static int edda () {
+        return 29;
+    }
+    public static void main(String args[]) {
+    }
+}
---
+++ src/test/java/TwentyNine.java
@@ -0,0 +1,9 @@
+package lateterm;
+
+public class TwentyNine {
+    public static int output() {
+        return 29;
+    }
+    public static void main(String args[]) {
+    }
+}
