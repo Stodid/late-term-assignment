5000d4226790e44a102f3065f4923587f49b7659
Eiríkur Orri Ólafsson <eirikurorri@gmail.com>
2014-10-27 17:06:58 +0000

    gradle builds and creates an app-0.1 jar file

--- src/main/java/TwentyNine.java
+++ src/main/java/TwentyNine.java
@@ -4,6 +4,11 @@ public class TwentyNine {
     public static int output() {
         return 29;
     }
+
     public static void main(String args[]) {
+        TwentyNine tn = new TwentyNine();
+        System.out.println("this is such a test now");
+        output();
     }
 }
+
