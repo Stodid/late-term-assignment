0311679530d1a51b9a2f542e397fe6bc2e1dd15f
Palli Moon <gunnarpg13@ru.is>
2014-10-30 12:18:51 +0000

    Heroku test

--- src/main/java/is/stodid/tictactoe/HelloWorld.java
+++ src/main/java/is/stodid/tictactoe/HelloWorld.java
@@ -1,7 +1,22 @@
-import static spark.Spark.*;
+package is.stodid.tictactoe;
 
-public class HelloWorld {
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+import spark.servlet.SparkApplication;
+
+public class HelloWorld implements SparkApplication{ 
+    public void init() {
+        Spark.get(new Route("/hello") {
+            @Override
+            public Object handle(Request request, Response response) {
+                return "Hello World!";
+            }
+        });
+    }
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+        SparkApplication hello = new HelloWorld();
+        hello.init();
     }
 }
