fb047c3f838058f5e45f5b843913eb013792a17d
Palli Moon <gunnarpg13@ru.is>
2014-10-30 18:14:03 +0000

    Freemarker i orden (hopefully)

--- src/main/java/is/stodid/tictactoe/TicWeb.java
+++ src/main/java/is/stodid/tictactoe/TicWeb.java
@@ -1,11 +1,17 @@
 package is.stodid.tictactoe;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import spark.*;
 import static spark.Spark.*;
 import spark.servlet.SparkApplication;
+import spark.template.freemarker.*;
+import spark.ModelAndView;
 
 public class TicWeb implements SparkApplication{ 
     public static void main(String[] args) {
+        staticFileLocation("/public");
         SparkApplication tic = new TicWeb();
         String port = System.getenv("PORT");
         if (port != null) {
@@ -18,6 +24,15 @@ public class TicWeb implements SparkApplication{
             @Override
             public Object handle(Request request, Response response) {
                 return "Tic!";
+            }
+        });
+
+        get(new FreeMarkerRoute("/fr") {
+            @Override
+            public ModelAndView handle(Request request, Response response) {
+                Map<String, String> result = new HashMap<String, String>();
+                result.put("one", "asdfasdf");
+                return modelAndView(result, "asdf.ftl");
             }
         });
     }
