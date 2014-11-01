f43a6ef4c08171c59fbe72ce6efef72cc4efeb2b
edda <eddapetursdottir@Eddas-MacBook-Pro-2.local>
2014-11-01 18:25:48 +0000

    Created empty constructor for Board

--- src/main/java/is/stodid/tictactoe/Board.java
+++ src/main/java/is/stodid/tictactoe/Board.java
@@ -2,8 +2,13 @@ package is.stodid.tictactoe;
 
 public class Board{
 
+	private char[][] board;
+
 	public Board(){
-
+		for (int i = 0; i < 3; i++){
+			for (int j = 0; i < 3; i++){
+				board[i][j] = ' ';
+			}
+		}
 	}
-
 }
