669641a5f8843a1425897bd0a3ba6076d12cf027
edda <eddapetursdottir@Eddas-MacBook-Pro-2.local>
2014-11-02 03:51:48 +0000

    Fiktað í Game og GameTest

--- src/main/java/is/stodid/tictactoe/Game.java
+++ src/main/java/is/stodid/tictactoe/Game.java
@@ -2,21 +2,22 @@ package is.stodid.tictactoe;
 
 public class Game {
     private Board b;
-    private Player player1;
-    private Player player2;
-    private int playerNr = 1;
+   // private Player player1;
+   // private Player player2;
+    private int playerNr;
     private int squareNr = 1;
 
     public Game() {
         b = new Board();
-        initPlayers();
-        Play();
+		playerNr = 1;
+        //initPlayers();
+		// Play();
     }
 
-    public void initPlayers() {
+   /* public void initPlayers() {
         this.player1 = new Player(1);
         this.player2 = new Player(2);
-    }
+    }*/
 
     public void Play() {
         while(b.isWinner() == 0 || !b.isBoardFull()) {
@@ -26,20 +27,27 @@ public class Game {
 			else 
 				continue;
         }
-
         if(b.isWinner() == 0 && b.isBoardFull()) {
             System.out.println("Draw!");
+		//	return false;
         }
         else {
             int winner = b.isWinner();
             System.out.println("Player : " + winner + "has won");
-        }
+		//	return false;
+		}
     }
 
-    public int switchPlayer(int playerNr) {
+    public static int switchPlayer(int playerNr) {
         if(playerNr == 1)
             return 2;
         else
             return 1;
     }
+	
+/*	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		Game g = new Game();
+				
+		}	*/
 }
