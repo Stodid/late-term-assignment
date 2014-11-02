71199f84925ce8cdbce59ae2169b4876350c8904
Palli Moon <gunnarpg13@ru.is>
2014-11-02 14:01:46 +0000

    Correct marks will now show

--- src/main/java/is/stodid/tictactoe/TicTacToe.java
+++ src/main/java/is/stodid/tictactoe/TicTacToe.java
@@ -9,6 +9,18 @@ public class TicTacToe {
         game = new Game();
     }
 
+    private static String numtomark(int num) {
+        switch (num) {
+            case 0:
+                return "&nbsp;";
+            case 1:
+                return "x";
+            case 2:
+                return "o";
+        }
+        return "&nbsp;";
+    }
+
     public static void in(String square) {
         game.in(Integer.parseInt(square));
     }
@@ -16,15 +28,15 @@ public class TicTacToe {
     public HashMap<String, String> out() {
         HashMap<String, String> out = new HashMap<String, String>();
         Board b = game.out();
-        out.put("zero",  String.valueOf(b.board[0][0]));
-        out.put("one",   String.valueOf(b.board[0][1]));
-        out.put("two",   String.valueOf(b.board[0][2]));
-        out.put("three", String.valueOf(b.board[1][0]));
-        out.put("four",  String.valueOf(b.board[1][1]));
-        out.put("five",  String.valueOf(b.board[1][2]));
-        out.put("six",   String.valueOf(b.board[2][0]));
-        out.put("seven", String.valueOf(b.board[2][1]));
-        out.put("eight", String.valueOf(b.board[2][2]));
+        out.put("zero",  String.valueOf(numtomark(b.board[0][0])));
+        out.put("one",   String.valueOf(numtomark(b.board[0][1])));
+        out.put("two",   String.valueOf(numtomark(b.board[0][2])));
+        out.put("three", String.valueOf(numtomark(b.board[1][0])));
+        out.put("four",  String.valueOf(numtomark(b.board[1][1])));
+        out.put("five",  String.valueOf(numtomark(b.board[1][2])));
+        out.put("six",   String.valueOf(numtomark(b.board[2][0])));
+        out.put("seven", String.valueOf(numtomark(b.board[2][1])));
+        out.put("eight", String.valueOf(numtomark(b.board[2][2])));
 
         return out;
     }
